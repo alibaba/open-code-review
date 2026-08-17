8ff873e09035b8091e1827a8e8db94acb3cafc08
wu21-web <wu2196674@icloud.com>
2026-08-17 19:27:33 +0900

    chore: new extensions for `.ipynb`

--- internal/config/allowlist/allowed_ext_test.go
+++ internal/config/allowlist/allowed_ext_test.go
@@ -63,6 +63,8 @@ func TestIsAllowedExt(t *testing.T) {
 		{".NIMS", true},
 		{".nimble", true},
 		{".NIMBLE", true},
+		{".ipynb", true},
+		{".IPYNB", true},
 		{".txt", false},
 		{".md", false},
 		{".png", false},
